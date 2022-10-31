36a79e32f8bab8763ec05f7975cb162f2a92b5f1
Ilson Warmling <ilsonwarsc@gmail.com>
2022-10-31 14:27:48 -0300

    feat/boolean-e-condicoes

--- boolean-e-condicionais/script.js
+++ boolean-e-condicionais/script.js
@@ -21,3 +21,38 @@ if(nome){
 if(!possuiGraduacao){
   console.log('Não possui graduação');
 }
+
+if((5-5) && (5+5)){
+  console.log('Verdadeiro');
+}else{
+  console.log('Falso');
+}
+
+var condicional = (5-10) && (5+5)
+if(condicional){
+  console.log('Verdadeiro', condicional);
+}else{
+  console.log('Falso');
+}
+
+// Operador || ou
+
+var condicional2 = (5-5 )||(5+5) || (10-2);
+console.log(condicional2);
+
+//operador switch
+
+var corDFavorita = 'Vermelho';
+switch(corDFavorita){
+case 'Azul':
+  console.log('Olhe para o céu');
+  break;
+  case 'Amarelo':
+    console.log('Olhe para o sol');
+    break;
+    case 'Vermelho':
+      console.log('Olhe para as rosas');
+      break;
+      default:
+        console.log('Feche os olhos');
+}
