1e13747391ae6a442de58ac594de27e8ec25a39d
Ilson Warmling <ilsonwarsc@gmail.com>
2022-10-31 13:09:55 -0300

    feat/boolean-e-condicionais

---
+++ boolean-e-condicionais/script.js
@@ -0,0 +1,23 @@
+var possuiGraduacao = true;
+var possuiDoutorado = false;
+
+if(possuiDoutorado) {
+  console.log('Possui graduação e doutorado');
+} else if(possuiGraduacao) {
+  console.log('Possui graduação, mas não possui doutorado');
+} else {
+  console.log('Não possui graduação');
+}
+// retorna Possui Graduação, mas não possui doutorado
+
+var nome = 'André';
+
+if(nome){
+  console.log(nome);
+} else {
+  console.log('nome nao existe ')
+}
+
+if(!possuiGraduacao){
+  console.log('Não possui graduação');
+}
