edd0b7d08db99ffac8d90c2d643924b836a44028
Ilson Warmling <ilsonwarsc@gmail.com>
2022-10-31 10:50:25 -0300

    feat/tipos-de-variaveis

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,19 @@
+var idade = 25;
+var simbolo = Symbol();
+console.log(typeof simbolo);
+
+var nome = "João";
+var sobreNome = "Rangel";
+var nomeCompleto = nome + " " + sobreNome;
+
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase = 'Romário fez ' + gols + ' gols';
+console.log(frase);
+
+var ano = 2018;
+var mes = 8;
+console.log(ano + mes); 
+
+var frase1 = "Esse é o \"melhor\" jogo";
