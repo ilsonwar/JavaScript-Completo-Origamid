970137c34298428c447885e42985ca95d723dbd1
Ilson Warmling <ilsonwarsc@gmail.com>
2022-10-31 10:46:38 -0300

    feat/variaveis

---
+++ variaveis/ex001/script.js
@@ -0,0 +1,13 @@
+// Declarar uma variável com o seu nome
+var nome = "Ilson";
+// Declarar uma variável com a sua idade
+var idade = 21;
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+// Atribuir valor a sua comida favorita
+comidaFavorita = "lasanha";
+// Declarar 5 variáveis diferentes sem valores
+var time, professor, endereco, rua, cep;
+
+console.log(nome, idade, comidaFavorita, time, professor, endereco, rua, cep);
---
+++ variaveis/script.js
@@ -0,0 +1,15 @@
+var nome = "andré";
+var idade = 23;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+console.log(totalPreco);
+
+var sobrenome,cidade;
+sobrenome = "silva";
+cidade = "são paulo";
+console.log(nome,sobrenome, cidade);
