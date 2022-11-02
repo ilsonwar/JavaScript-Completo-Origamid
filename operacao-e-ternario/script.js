fbf3ecdc2bebdfb630a5f99d381c566ddd035afd
Ilson Warmling <ilsonwarsc@gmail.com>
2022-11-02 11:18:53 -0300

    feat/Atribuicao-e-ternarios

---
+++ operacao-e-ternario/script.js
@@ -0,0 +1,20 @@
+var x = 5;
+var y = 10;
+x += y; // x = x + y (15)
+x -= y; // x = x - y (-5)
+x *= y; // x = x * y (50)
+x /= y; // x = x / y (0.5)
+x %= y; // x = x % y (0)
+x **= y; // x = x ** y (9765625)
+
+var numero = 80;
+
+numero +=  10; //numero = numero + 10
+console.log(numero);
+
+
+//ternário
+var idade = 20;
+var podeBeber = (idade >= 18) ? 'Pode beber' : 'Não pode beber';
+console.log(podeBeber);
+
